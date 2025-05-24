d45f4d61e2b384283c2b01e76d967d14d66031de
asivakiran <asivakiran@gmail.com>
2025-05-24 18:00:18 +0100

    Dependencies changed for PDF

--- src/main/java/com/itccompliance/order/service/PdfGeneratorService.java
+++ src/main/java/com/itccompliance/order/service/PdfGeneratorService.java
@@ -1,13 +1,17 @@
 package com.itccompliance.order.service;
 
 import com.itccompliance.order.entity.Order;
-import org.apache.poi.xwpf.usermodel.*;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.InputStream;
 
 @Service
@@ -28,21 +32,29 @@ public class PdfGeneratorService {
                 for (XWPFRun run : paragraph.getRuns()) {
                     String text = run.getText(0);
                     if (text != null) {
-                        text = text.replace("{{customerName}}", order.getCustomerName())
-                                .replace("{{totalPrice}}", String.valueOf(order.getTotalPrice()))
-                                .replace("{{status}}", order.getStatus());
+                        text = text.replace("customerName", order.getCustomerName())
+                                .replace("totalPrice", String.valueOf(order.getTotalPrice()))
+                                .replace("orderStatus", order.getStatus());
                         run.setText(text, 0);
                     }
                 }
             }
 
-            File outFile = File.createTempFile("order_" + order.getId(), ".docx");
-            try (FileOutputStream out = new FileOutputStream(outFile)) {
-                document.write(out);
+            PdfWriter pdfWriter = new PdfWriter("order_" + order.getId()+ ".pdf");
+            PdfDocument pdf = new PdfDocument(pdfWriter);
+            Document document1 = new Document(pdf);
+            for(XWPFParagraph para : document.getParagraphs()){
+                document1.add(new Paragraph(para.getText()));
             }
 
-            logger.info("Generated document for order {} at {}", order.getId(), outFile.getAbsolutePath());
+            /*File outFile = File.createTempFile("order_" + order.getId(), ".docx");
+            try (FileOutputStream out = new FileOutputStream(outFile)) {
+                document.write(out);
+            }*/
 
+            logger.info("Generated document for order {} at {}", order.getId(), "order_" + order.getId()+ ".pdf");
+            document1.close();
+            pdf.close();
         } catch (Exception e) {
             logger.error("Failed to generate PDF for order {}", order.getId(), e);
         }
